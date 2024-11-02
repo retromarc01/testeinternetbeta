9e25b4c384d0e493ee69281cce51c871baefd343
retromarc01 <gouvmarc000@gmail.com>
2024-11-02 16:17:08 -0300

    estruturando framework

--- teste_internet_app/__init__.py
+++ teste_internet_app/__init__.py
@@ -2,7 +2,6 @@ from teste_internet_app.screens.main_screen import MainScreen
 from teste_internet_app.screens.myscreen_manager import MyScreenManager
 from kivy_reloader.app import App
 from kivymd.app import MDApp
-
 from teste_internet_app.controller.main_controller import MainController
 from teste_internet_app.model.database import Database
 from kivy.clock import Clock
@@ -16,14 +15,14 @@ class MainApp(App,MDApp):
         #self.view = MainScreen(self.controller)
         self.view = MyScreenManager(self.controller)#(self.controller)
         self.controller.view = self.view
+        self.theme_cls.theme_style_switch_animation = True
+        self.theme_cls.theme_style_switch_animation_duration = 0.8
         self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_hue = "600"
         self.theme_cls.primary_palette = "Purple"
-        
         #self.controller = MainController(self.view)
         self.db = Database('my_database.db')
         #print(self.controller.data)
         self.db.create_table()
- 
+        
         return self.view
-
-
